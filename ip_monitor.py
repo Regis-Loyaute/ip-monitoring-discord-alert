e7785c1f9a568db4979627dcca9617a065f826f3
Régis <84942989+Regis-Loyaute@users.noreply.github.com>
2023-04-29 02:39:46 +0200

    ping retries + config.ini

--- ip_monitor.py
+++ ip_monitor.py
@@ -2,11 +2,16 @@
 import time
 import requests
 import logging
+import configparser
 from datetime import datetime
 from ping3 import ping
 
-ip_address = "10.0.0.2"
-webhook_url = "https://discord.com/api/webhooks/1096581762450210976/B1v-ofdsfsdf3yPMy_GYfsdfgsdfdsfsdngj44CXDT3AaR5454534534uk2DzSD_FWAmWJ1twt_4Wy59aCx"
+# Read configuration
+config = configparser.ConfigParser()
+config.read("config.ini")
+ip_address = config.get("settings", "ip_address")
+webhook_url = config.get("settings", "webhook_url")
+
 last_ping_time = datetime.now()
 is_up = True
 
@@ -31,27 +36,43 @@ def send_webhook_message(content):
     except requests.exceptions.RequestException as e:
         logger.error(f"Error sending webhook: {e}")
 
-while True:
-    try:
-        response_time = ping(ip_address)
-    except OSError as e:
-        logger.error(f"Ping error: {e}")
-        response_time = None
+def downtime_seconds(last_ping_time, current_time):
+    downtime = current_time - last_ping_time
+    return downtime.total_seconds()
 
-    if response_time is not None:
-        if not is_up:
-            now = datetime.now()
-            downtime = now - last_ping_time
-            downtime_seconds = downtime.total_seconds()
-            message = f"{ip_address} is up! It was down for {downtime_seconds:.2f} seconds."
-            logger.info(message)
-            send_webhook_message(message)
-            is_up = True
-        last_ping_time = datetime.now()
-    else:
-        if is_up:
+def ping_with_retries(ip, max_retries=3, delay_between_retries=2):
+    for attempt in range(1, max_retries + 1):
+        try:
+            response_time = ping(ip)
+            return response_time
+        except Exception as e:
+            logger.debug(f"Ping error on attempt {attempt}: {e}")
+            if attempt < max_retries:
+                time.sleep(delay_between_retries)
+            else:
+                return None
+
+def main():
+    global last_ping_time, is_up
+    while True:
+        response_time = ping_with_retries(ip_address)
+
+        if response_time is not None:
+            if not is_up:
+                now = datetime.now()
+                downtime_secs = downtime_seconds(last_ping_time, now)
+                message = f"{ip_address} is up! It was down for {downtime_secs:.2f} seconds."
+                logger.info(message)
+                send_webhook_message(message)
+                is_up = True
             last_ping_time = datetime.now()
-            is_up = False
-            logger.warning(f"{ip_address} is down")
+        else:
+            if is_up:
+                last_ping_time = datetime.now()
+                is_up = False
+                logger.warning(f"{ip_address} is down")
 
-    time.sleep(10)
+        time.sleep(10)  # Ping every 10 seconds
+
+if __name__ == '__main__':
+    main()
